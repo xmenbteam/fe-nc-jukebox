c0aa4c32a20e723349608c932b669b5f973d3eeb
Sam Parry <67956822+xmenbteam@users.noreply.github.com>
2022-04-06 08:49:12 +0100

    Sledgehammer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 const App = () => {
-  const playListTitle = 'My cool playlist';
+  const playListTitle = "My cool playlist";
   const songs = [
     {
       id: 1,
-      title: 'Dreams',
-      artist: 'Fleetwood Mac',
+      title: "Dreams",
+      artist: "Fleetwood Mac",
       albumCover:
-        'https://upload.wikimedia.org/wikipedia/en/f/fb/FMacRumours.PNG',
+        "https://upload.wikimedia.org/wikipedia/en/f/fb/FMacRumours.PNG",
       previewURL:
-        'https://open.spotify.com/embed/track/0ofHAoxe9vBkTCp2UQIavz?utm_source=generator',
+        "https://open.spotify.com/embed/track/0ofHAoxe9vBkTCp2UQIavz?utm_source=generator",
     },
     {
       id: 2,
       title: "Don't stop me now",
-      artist: 'Queen',
+      artist: "Queen",
       albumCover:
-        'https://upload.wikimedia.org/wikipedia/en/9/97/Queen-dont-stop-me-now-1979-6-s.jpg',
+        "https://upload.wikimedia.org/wikipedia/en/9/97/Queen-dont-stop-me-now-1979-6-s.jpg",
       previewURL:
-        'https://open.spotify.com/embed/track/5T8EDUDqKcs6OSOwEsfqG7?utm_source=generator',
+        "https://open.spotify.com/embed/track/5T8EDUDqKcs6OSOwEsfqG7?utm_source=generator",
     },
     {
       id: 3,
-      title: 'Enter Sandman',
-      artist: 'Metallica',
+      title: "Enter Sandman",
+      artist: "Metallica",
       albumCover:
-        'https://upload.wikimedia.org/wikipedia/en/2/2c/Metallica_-_Metallica_cover.jpg',
+        "https://upload.wikimedia.org/wikipedia/en/2/2c/Metallica_-_Metallica_cover.jpg",
       previewURL:
-        'https://open.spotify.com/embed/track/5sICkBXVmaCQk5aISGR3x1?utm_source=generator',
+        "https://open.spotify.com/embed/track/5sICkBXVmaCQk5aISGR3x1?utm_source=generator",
+    },
+    {
+      id: 4,
+      title: "Sledgehammer",
+      artist: "Peter Gabriel",
+      albumCover:
+        "https://upload.wikimedia.org/wikipedia/en/c/cf/Sledgehammer_Cover.jpg",
+      previewURL:
+        "https://open.spotify.com/track/029NqmIySn1kOY305AAhxT?si=a36b7d4e3f324363",
     },
   ];
   const playCounts = {
     Dreams: 10,
-    'Enter Sandman': 5,
+    "Enter Sandman": 5,
     "Don't stop me now": 8,
   };
   const currentlyPlaying = "Don't stop me now";
